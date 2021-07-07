dacd426e58d6a16e6ab665456fcc8392474e76c9
Nico Romeo <nromeo@mit.edu>
2021-07-07 12:12:02 -0400

    Update streamintegrators.py

--- scripts/streamintegrators.py
+++ scripts/streamintegrators.py
@@ -1,7 +1,6 @@
 import numpy as np
 import numba as nb
 from numba import jit,prange
-#import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 from dedalus.core import coords, distributor, basis, field, operators, arithmetic
 #from dedalus.extras.flow_tools import GlobalArrayReducer
@@ -16,7 +15,9 @@ import time
 
 @jit(nopython=True,parallel=True)
 def compute_link_DS(l,k):
-    """ Computes the linking number between 2 polylines - uses JIT compiling via numba and auto-parallelization to get faster """
+    """ Computes the linking number between 2 polylines 
+    - uses JIT compiling via numba and auto-parallelization for faster execution 
+    """
     λ = 0
     Nl = l.shape[0]
     Nk = k.shape[0]
@@ -45,29 +46,12 @@ def compute_link_DS(l,k):
     return λ/(2*np.pi)
 
 
-def generate_streamlines(N_sample, num_cores, fun):
-      # sample N_sample random points uniformly inside the ball (Polar sampling method)
-    u = 2 * np.random.rand(N_sample) - 1
-    p = 2*np.pi * np.random.rand(N_sample)
-    r = np.random.rand(N_sample) ** (1/3.)
-
-    x_0 = r * np.cos(p) * np.sqrt(1-u*u)
-    y_0 = r * np.sin(p) * np.sqrt(1-u*u)
-    z_0 = r * u
-
-    points = [np.array((x_0[i],y_0[i],z_0[i])) for i in range(N_sample)]
-    #
-    #streamlines = Parallel(n_jobs=num_cores)(delayed(fun)(x) for x in points)
-
-    streamlines = []
-    for x in points:
-        streamlines.append(fun(x))
-    return streamlines
-
-
-
 class StreamGen:
-
+    """
+    Class containing the velocity field at time t, computes vorticity and estimates helicity through streamline linking number computation
+    
+    
+    """
     def __init__(self, num_cores, N_rounds, N_sample, T, ug):
         self.num_cores = num_cores
         self.N_rounds = N_rounds
@@ -104,20 +88,22 @@ class StreamGen:
         self.H_op = dot(u, self.ω)
 
 
-
     def wrap_ω_interp(self, t, x):
         return self.ω_interp(x)
 
     def integrate_streamline(self, x0):
         RHS = self.wrap_ω_interp
         t_bounds = [0, self.T]
-        rtol = 1e-3
+        rtol = 1e-3 # test integration parameters
         atol = 1e-4
         sol = solve_ivp(self.wrap_ω_interp, t_bounds, x0, rtol=rtol, atol=atol, dense_output=True, max_step=1e-2)
         return sol
 
     def generate_streamlines_method(self):
-        # sample N_sample random points uniformly inside the ball (Polar sampling method)
+        """
+        Generates N_sample streamlines - not functional as parallel code
+        """
+        # samples N_sample random points uniformly inside the ball (Polar sampling method)
         u = 2 * np.random.rand(N_sample) - 1
         p = 2*np.pi * np.random.rand(N_sample)
         r = np.random.rand(N_sample) ** (1/3.)
@@ -128,11 +114,11 @@ class StreamGen:
 
         points = [np.array((x_0[i],y_0[i],z_0[i])) for i in range(N_sample)]
 
-        #
-        streamlines = Parallel(n_jobs=self.num_cores)(delayed(self.integrate_streamline)(x) for x in points)
-
-
-
+        # 
+        #streamlines = Parallel(n_jobs=self.num_cores)(delayed(self.integrate_streamline)(x) for x in points)
+        streamlines = []
+        for x in points:
+            streamlines.append(self.integrate_streamline(x))
         return streamlines
 
 
@@ -141,17 +127,20 @@ class StreamGen:
         hel_estimates_seq = [0]
         sample_times = np.linspace(0,self.T, 100)
         for _ in range(self.N_rounds):
-            #streamlines = self.generate_streamlines_method()
-            streamlines = generate_streamlines(self.N_sample, self.num_cores, self.integrate_streamline)
+            # generates N_sample streamlines
+            streamlines = self.generate_streamlines()
+            #discretize streamlines by naive segment evaluation.
             discrete_stream = []
             for i in range(N_sample):
                 discrete_stream.append(streamlines[i].sol(sample_times).T)
+            # linking number computation
             for i in range(N_sample):
-                l = discrete_stream[i]
+                l = discrete_stream[i] 
                 print(l.shape)
                 for j in range(0, i-1):
-                    #discretize streamlines
-                    self.hel_est += 2 * compute_link_DS(l,discrete_stream[j])
+                    self.hel_est += 2 * compute_link_DS(l,discrete_stream[j]) 
+                    # the factor 2 comes for counting the integral twice for (i,j) and (j,i).
+                    # unsure whether it is actually required.
                     self.hel_est /= self.T*self.T
-                    hel_estimates_seq.append(self.hel_est)
+                    hel_estimates_seq.append(self.hel_est) # keeps track of the running value of the helicity estimate
         return hel_estimates_seq
