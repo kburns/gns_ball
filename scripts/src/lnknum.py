7388ca8ada8a0cdcc9fdba8bed890fbc1431c362
Keaton J. Burns <keaton.burns@gmail.com>
2021-07-22 17:10:16 -0400

    Modify linking number functions to interface with streamline objects

--- scripts/src/lnknum.py
+++ scripts/src/lnknum.py
@@ -1,36 +1,45 @@
+
+
 import numpy as np
 from numba import jit, prange
 
 
+def compute_link_DS(s1, s2):
+    """Compute linking number between two Streamline objects."""
+    # Make closed polylines from streamline nodes
+    x1 = np.vstack((s1.x, s1.x[0:1]))
+    x2 = np.vstack((s2.x, s2.x[0:1]))
+    # Call compiled linking number computation
+    return _compute_link_DS(x1, x2)
+
+
+def compute_partial_link_DS(s1, s2, dL=1):
+    """Compute partial linking numbers between two Streamline objects."""
+    pass
+
+
 @jit(nopython=True, parallel=True)
-def compute_link_DS(l, k):
-    """ Computes the linking number between 2 polylines - uses JIT compiling
-    via numba and auto-parallelization to get faster 
-    l,k must have shape (Nl, 3) and (Nk, 3)
+def _compute_link_DS(ls, ks):
+    """
+    Compute linking number between two closed polylines with shape (N, 3).
+    The last point should equal the first point for each line.
     """
     λ = 0
-    Nl = l.shape[0]
-    Nk = k.shape[0]
-    # Add first point to close loop
-    ls0 = np.expand_dims(l[0, :], 0)
-    ks0 = np.expand_dims(k[0, :], 0)
-    ls = np.vstack((l, ls0))
-    ks = np.vstack((k, ks0))
-    for i in prange(Nk):
-        for j in range(Nl):
+    Nl = ls.shape[0]
+    Nk = ks.shape[0]
+    for i in prange(Nk-1):
+        for j in range(Nl-1):
             a = ls[j, :] - ks[i, :]
             b = ls[j, :] - ks[i + 1, :]
             c = ls[j + 1, :] - ks[i + 1, :]
             d = ls[j + 1, :] - ks[i, :]
-
             p = np.dot(a, np.cross(b, c))
             an = np.sqrt(np.dot(a, a))
             bn = np.sqrt(np.dot(b, b))
             cn = np.sqrt(np.dot(c, c))
             dn = np.sqrt(np.dot(d, d))
-
             d1 = an * bn * cn + np.dot(a, b) * cn + np.dot(b, c) * an + np.dot(c, a) * bn
             d2 = an * dn * cn + np.dot(a, d) * cn + np.dot(d, c) * an + np.dot(c, a) * dn
-
             λ += (np.arctan2(p, d1) + np.arctan2(p, d2))
     return λ / (2 * np.pi)
+
