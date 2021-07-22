4191bcdc5ec562bf3a71d8122da189f67f4a76ef
Keaton J. Burns <keaton.burns@gmail.com>
2021-07-22 15:41:42 -0400

    Rework streamline integration to only use normalized method

--- scripts/src/streamline.py
+++ scripts/src/streamline.py
@@ -2,58 +2,52 @@ import numpy as np
 from scipy.integrate import solve_ivp, quad
 
 class Streamline():
+    """Streamline integrator.
 
-    def __init__(self, x0, T, tols, interpolator, normalized=False, max_step=1e-3):
+    Parameters
+    ----------
+    x0 : ndarray with shape (3,)
+        Initial cartesian position.
+    L : float
+        Arclength for integration.
+    rtol, atol, max_Step : float
+        Parameters passed to scipy.integrate.solve_ivp.
+
+    Notes
+    -----
+    The streamline is integrated in arclength coordinates by normalizing the tangent vector
+    field supplied to the integrate method.
+    """
+
+    def __init__(self, x0, L, rtol, atol, max_step):
         self.x0 = x0
-        self.t_bound = T
-        self.rtol = tols[0]
-        self.atol = tols[1]
+        self.L = L
+        self.rtol = rtol
+        self.atol = atol
         self.max_step = max_step
-        self.interpolator = interpolator
-        self.normalized = normalized
 
-    def wrap_interp(self, t, x):
-        return self.interpolator(x)
+    def integrate(self, vector_interpolator):
+        dy = np.zeros(5)
+        # Define integration RHS
+        def RHS(s, y):
+            vector = vector_interpolator(y[0:3])[0]
+            vector_mag = np.linalg.norm(vector)
+            np.divide(vector, vector_mag, out=dy[0:3])
+            dy[3] = vector_mag
+            dy[4] = 1 / vector_mag
+            return dy
+        # Setup extended initial conditions
+        y0 = np.append(self.x0, [0, 0])
+        # Integrate
+        self.sol = sol = solve_ivp(RHS, (0, self.L), y0, rtol=self.rtol, atol=self.atol,
+                                   dense_output=True, max_step=self.max_step)
+        if sol.status != 0:
+            raise ValueError('solve_ivp did not converge')
+        # References
+        self.s = sol.t
+        self.x = sol.y[0:3].T
+        self.C = sol.y[3]
+        self.T = sol.y[4]
+        self.C_final = self.C[-1]
+        self.T_final = self.T[-1]
 
-    def wrap_interp_normalized(self, t, x):
-        ωi = self.interpolator(x[0:3])[0]
-        #ωi = self.interpolator(x)
-        ω_mag = np.linalg.norm(ωi)
-        #return ωi / ω_mag
-        return np.append(ωi / ω_mag, ω_mag)
-
-    def integrate(self):
-        # Define integrator function: if normalized
-        if self.normalized:
-            self.RHS = self.wrap_interp_normalized
-            x0s = np.hstack((self.x0, 0.)) # integrate circulation simulteneaously
-            #x0s = self.x0
-        else:
-            self.RHS = self.wrap_interp
-            x0s = self.x0
-            # Integrate streamline
-        sol = solve_ivp(self.RHS,
-                        (0, self.t_bound),
-                        x0s,
-                        rtol=self.rtol,
-                        atol=self.atol,
-                        dense_output=True,
-                        max_step=self.max_step)
-        self.status = sol.status
-        if self.status < 0:
-            raise ValueError('Integrator not converged')
-        if self.normalized:
-            # integrate circulation  C = \int_0^t_max np.dot(omega(r(t)), dr(t)), with dr = RHS dt
-            # Note that in the normalized case, t has units of length.
-            # = norm of interpolator
-
-            C = sol.y[-1,:] # total circulation
-            L = self.t_bound  # Arc length of streamline: exactly known
-            self.C = C
-            self.y = sol.y[0:3,:].T
-            self.T = L / C[-1]
-
-        else:  # unnormalized case: t has units of time.
-            self.T = self.t_bound
-            self.y = sol.y.T
-        self.sol = sol
