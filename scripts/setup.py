cc2db9c68cf8cb54e62e08308031fc5707bd3354
Keaton J. Burns <keaton.burns@gmail.com>
2021-08-05 15:41:38 -0400

    Add package files

---
+++ scripts/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name="dedalus_linking",
+    version="0.0.1",
+    description="Active matter analysis tools for the ball.",
+    python_requires=">=3.6",
+)
