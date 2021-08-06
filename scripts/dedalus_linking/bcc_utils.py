18069d67bd358fe120fbf189787d8b5288b994b6
NicoRomeo <nromeo@mit.edu>
2021-08-06 14:27:10 -0400

    fixed one bug, introduced import bcc funciton, and read from FNL output parser function

--- scripts/dedalus_linking/bcc_utils.py
+++ scripts/dedalus_linking/bcc_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.sparse as sp
 
 def _create_header(Nc, TC):
     """
@@ -49,12 +50,11 @@ def _create_datablock(curves, closed=True):
                 raise ValueError("Invalid curve shape")
         curve = curve.astype(np.float32)
         if closed:
-            content_stream += (-curve.shape[1]).to_bytes(4,byteorder='little', signed=True)
+            content_stream += (-curve.shape[0]).to_bytes(4,byteorder='little', signed=True)
         else:
-            content_stream += (curve.shape[1]).to_bytes(4,byteorder='little', signed=True)
+            content_stream += (curve.shape[0]).to_bytes(4,byteorder='little', signed=True)
         content_stream += curve.ravel().tobytes() # implicitely uses 'C' memory layout order 
-        
-        return content_stream
+    return content_stream
     
 def export_closed_BCC(filename, arr_list):
     """
@@ -85,5 +85,48 @@ def export_closed_BCC(filename, arr_list):
             file.write(byte.to_bytes(1, byteorder='little'))
     print("{0} curves with {1} total points succesfully exported to bcc".format(Nc, TC))
     
+def read_BCC(filename):
+    if filename[-4:] != '.bcc':
+        filename += '.bcc'
+    with open(filename, "rb") as file:
+        head = file.read(64)
+        
+        if head[0:3] != b'BCC':
+            raise ValueError("File is not a Binary Curve Collection (BCC)")
 
+        Ncurves = int.from_bytes(head[8:16], byteorder="little", signed=False)
+        Tot_points = int.from_bytes(head[16:24], byteorder="little", signed=False)
+
+        is_closed = np.zeros(Ncurves)
+        len_curves = np.zeros(Ncurves, dtype=int)
+        list_curves = []
+        for i in range(Ncurves):
+            print(i)
+            len_curves[i] = int.from_bytes(file.read(4), byteorder="little", signed=True)
+            if len_curves[i] < 0: # if curve is closed
+                is_closed[i] = 1
+                len_curves[i] *= -1
+            curve = np.frombuffer(file.read(12*len_curves[i]), dtype=np.float32, count=-1)
+            list_curves.append(np.reshape(curve, (len_curves[i], 3)))
+        chunk = file.read(1) 
+        if chunk != b'':
+            raise ValueError("Error loading")
+        return list_curves, is_closed
     
+def readtxt_certificate(filename, sparse=False):
+    if filename[-4:] != '.txt':
+        filename += '.txt'
+    with open(filename, 'r') as file:
+        Ncurve = int(file.readline())
+        if sparse:
+            lnks = sp.dok_matrix((Ncurve, Ncurve), dtype=np.int32)
+        else:
+            lnks = np.zeros((Ncurve, Ncurve))
+        for l in file:
+            i,j, lnk = l.split(',')
+            i = int(i)
+            j = int(j)
+            if i < j:
+                i,j = j,i
+            lnks[i, j] = int(lnk)
+    return lnks
